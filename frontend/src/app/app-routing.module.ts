db46b992d0a2613f74ea42e742fb4eeb6c5e3a41
Saúl García Céspedes <sgarcc17@estudiantes.unileon.es>
2022-04-23 13:51:56 +0200

    Initial Angular project

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module')
+        .then((m) => m.HomeModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+/**
+ * Root Angular routing module
+ */
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+
+/**
+ * Root Angular component
+ */
+export class AppComponent {
+  title = 'portfolio';
+}
