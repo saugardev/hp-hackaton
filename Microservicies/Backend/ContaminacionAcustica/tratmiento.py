e1a775cd3a2718697f57bdb20948410b5ce62fc5
jblant00 <jblant00@estudiantes.unileon.es>
2022-04-23 20:17:11 +0200

    Microservicio Lectura Contaminacion Acustica hecha

---
+++ Microservicies/Backend/ContaminacionAcustica/tratmiento.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+
+import os
+
+import pymongo
+
+import json
+
+
+
+#Guardamos la ubicación del archivo
+from bson import encode
+from bson import BSON
+from pandas import DataFrame
+
+filePath = os.path.abspath(__file__)
+print(filePath)
+#Guardamos la direccion del archivo al que queremos acceder
+filePath += "/../../../../Datasets/Anthem_CTC_ContaminacionAcustica.csv"
+dataset = pd.read_csv(filePath, sep=";", encoding="unicode_escape")
+#Borramos columnas
+dataset = pd.DataFrame(dataset)
+dataset.drop(['LAS01', 'LAS10', 'LAS50', 'LAS90', 'LAS99'], axis=1, inplace=True)
+
+
+#Generamos los documentos para guardar en la base de datos
+data = dataset.to_json(orient="records")
+print(data)
+
+myClient = pymongo.MongoClient("mongodb+srv://root:root@cluster0.jntcr.mongodb.net/Anthem?retryWrites=true&w=majority")
+
+#mycol= myClient.Anthem.ContAcus
+#with open('data.json') as jsonfile:
+    #c = json
+    #mycol.insert_many(c)
