831d0f0bd58d352304580e8151ab2ff8fd8580fe
Saúl García Céspedes <saul.garces09@gmail.com>
2022-04-23 16:47:18 +0200

    Basic page structure

--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -1,3 +1,4 @@
+import { CoreModule } from './../../core/core.module';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
@@ -11,6 +12,7 @@ import {HomeComponent} from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    CoreModule
   ],
 })
 /**
