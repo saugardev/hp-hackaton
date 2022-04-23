d917aa94ffebadf242f314f446e3eba3d09daf00
Saúl García Céspedes <saul.garces09@gmail.com>
2022-04-23 18:03:51 +0200

    Fix package json error

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# HP TECH CHALLENGE
+# HP_TECH_CHALLENGE
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.
 
--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/HP TECH CHALLENGE'),
+      dir: require('path').join(__dirname, './coverage/HP_TECH_CHALLENGE'),
       subdir: '.',
       reporters: [
         { type: 'html' },
