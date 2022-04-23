5dc68f56f7a88bbab880c5e7561f75ceb296fa27
Saúl García Céspedes <saul.garces09@gmail.com>
2022-04-23 17:00:47 +0200

    Grafica de graficas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: '',
-    loadChildren: () =>
-      import('./modules/home/home.module')
-        .then((m) => m.HomeModule),
-  },
+  {path: '', loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule) },
+  { path: 'graph', loadChildren: () => import('./modules/graph/graph.module').then(m => m.GraphModule) },
 ];
 
 @NgModule({
