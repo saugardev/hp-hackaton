3d78df0df5338da8c561e0e125b6e89d106d1ff6
Saúl García Céspedes <saul.garces09@gmail.com>
2022-04-23 16:16:24 +0200

    Material module

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MaterialModule
   ],
+  exports: [
+    MaterialModule
+  ]
 })
 /**
  * CoreModule class
