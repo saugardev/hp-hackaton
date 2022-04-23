69bed6da7b5920bacf8422b2c8e5534d34c2d2f7
sergiods00 <47632500+sergiods00@users.noreply.github.com>
2022-04-23 21:45:50 +0200

    Dataset trafico conectado

---
+++ Microservicies/Backend/Trafico/trafico.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import pymongo
+
+#Conexion con mongo.
+
+client = pymongo.MongoClient("mongodb+srv://root:root@cluster0.jntcr.mongodb.net/Anthem?retryWrites=true&w=majority")
+myCol = client.Anthem.Trafico
+
+#Obtencion del datashet a partir del archivo python
+
+filePath = os.path.abspath(__file__)
+datasetTraficoPath = os.chdir(filePath + "/../../../../Datasets/Trafico")
+files = os.listdir(datasetTraficoPath)
+
+csvMedidaTrafico = pd.read_csv(filePath + "/../../../../Datasets/Ubicaciones/Anthem_CTC_PuntoMedidaTrafico.csv", sep=';')
+csvMedidaTraficoFiltrado = csvMedidaTrafico[csvMedidaTrafico["id"] <= 1050]
+
+#Recorrer todos los csv de trafico.
+
+for file in files:
+    if file.endswith(".csv"):
+        csvTrafico = pd.read_csv(file, sep=';', encoding="unicode_escape")
+        csvTraficoFiltrado = csvTrafico[csvTrafico["id"] <= 1050]
+        csvTraficoFiltrado["fecha"] = pd.to_datetime(csvTraficoFiltrado["fecha"], format="%Y-%m-%d", errors="coerce")
+        csvTraficoFiltrado = csvTraficoFiltrado.loc[csvTraficoFiltrado["fecha"].dt.day < 6]
+        #print(len(csvTraficoFiltrado))
+        jsonData = csvTraficoFiltrado.to_dict(orient="records")
+        #print(jsonData)
+
+        myCol.insert_many(jsonData)
