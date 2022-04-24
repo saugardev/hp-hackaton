bbdc6262538eb3e66aa91656ed722973365268bc
Saúl García Céspedes <saul.garces09@gmail.com>
2022-04-24 06:29:39 +0200

    Marker cluster

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -3,11 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet.utm';
-
-type coords = {
-  x: number;
-  y: number;
-}
+import 'leaflet.markercluster';
 
 @Component({
   selector: 'app-home',
@@ -21,6 +17,10 @@ export class HomeComponent implements AfterViewInit {
 
   private _map: L.Map | undefined;
   private _markers: L.Marker[] | undefined;
+  private markerCluster = (L as any).markerClusterGroup({
+    spiderfyOnMaxZoom: false,
+    disableClusteringAtZoom: 16
+  });
 
   public get map(): L.Map {
     return this._map as L.Map;
@@ -49,8 +49,8 @@ export class HomeComponent implements AfterViewInit {
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [39.8282, -98.5795],
-      zoom: 3,
+      center: [39.8282, -3.5795],
+      zoom: 6,
       attributionControl: false,
       boxZoom: true,
       maxBoundsViscosity: 0.6,
@@ -82,12 +82,16 @@ export class HomeComponent implements AfterViewInit {
   private waitForIcons(): void {
     this.httpService.icons.subscribe(res => {
       if (this.httpService.icons.value === "") return;
+      this.map.removeLayer(this.markerCluster);
+      this.markerCluster = (L as any).markerClusterGroup({
+        spiderfyOnMaxZoom: false,
+        disableClusteringAtZoom: 16
+      });
       const markerCoords = [];
       this.markers = []
       for (const datum of this.httpService.accidentablididad.value) {
         try {
           if (datum.distrito === res) {
-            console.log(res);
             const x = parseFloat(datum.coordenada_x_utm.replace(",", "."));
             const y = parseFloat(datum.coordenada_y_utm.replace(",", "."));
             const item = L.utm({x, y, zone: 30, southHemi: false, band: "N"});
@@ -115,8 +119,9 @@ export class HomeComponent implements AfterViewInit {
         const obj = new L.Marker([marker.x, marker.y], {
           icon: icon,
         });
-        this.markers?.push(obj.addTo(this.map));
+        obj.addTo(this.markerCluster);
       }
+      this.markerCluster.addTo(this.map);
     });
   }
 }
