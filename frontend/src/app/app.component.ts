a085bb15bcbec11d57ecc42915bdf44c319e4368
sergiods00 <47632500+sergiods00@users.noreply.github.com>
2022-04-24 11:12:51 +0200

    Sistema de notificaciones

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,30 @@ import {Component} from '@angular/core';
  */
 export class AppComponent {
   title = 'HP_TECH_CHALLENGE';
+
+  public notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Alerta: Varios accidentes en la misma via.\nAlerta: Nivel acústico demasiado alto.");
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Alerta: Varios accidentes en la misma via.\nAlerta: Nivel acústico demasiado alto.");
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
 }
